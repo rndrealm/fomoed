5810df165d985febc1ece0242db7bf159545d7c0
JakubBlaha-IDS <jakubblaha@ids.company>
2025-04-22 19:46:50 +0200

    feat: support widget embeds

---
+++ apps/dashboard/src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/ui/navbar";
+import { UserProvider } from "@/components/providers/UserProvider";
+
+export default function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <UserProvider>
+            <div className="absolute inset-x-0 top-0">
+                <Navbar />
+            </div>
+            {children}
+        </UserProvider>
+    );
+}
