c0b474581424244c3c50df2131432d058e3c1c5f
JakubBlaha-IDS <jakubblaha@ids.company>
2025-04-28 10:01:33 +0200

    fix: require use to log in when accessing smart signals dashbaord

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -2,26 +2,29 @@ import { NextResponse, type NextRequest } from "next/server";
 import { createSupabaseReqResClient } from "@/lib/utils/supabase/server-client";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+    const response = NextResponse.next({
+        request: {
+            headers: request.headers,
+        },
+    });
 
-  const supabase = await createSupabaseReqResClient(request, response);
+    const supabase = await createSupabaseReqResClient(request, response);
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
 
-  // protects the "/account" route and its sub-routes
-  if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+    // protects the "/dashboard" and "/signals" routes and their sub-routes
+    if (
+        !user &&
+        (request.nextUrl.pathname.startsWith("/dashboard") || request.nextUrl.pathname.startsWith("/signals"))
+    ) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-  return response;
+    return response;
 }
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*"],
+    matcher: ["/", "/dashboard/:path*", "/signals/:path*", "/signals"],
 };
