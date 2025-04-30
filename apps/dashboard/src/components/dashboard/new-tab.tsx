cee119e39bee189ca8134976243af83d77692c80
Seun Oyediran <oyediran@ids.company>
2025-04-30 22:49:18 +0100

    feat: add close tabs ui

--- apps/dashboard/src/components/dashboard/new-tab.tsx
+++ apps/dashboard/src/components/dashboard/new-tab.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { AddTab, TabLayout } from "../icons/icons";
+import React, { Fragment, useState } from "react";
+import { AddTab, CloseTab, TabLayout } from "../icons/icons";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import {
   activeTabAtom,
@@ -7,66 +7,97 @@ import {
   tabsAtom,
 } from "@/lib/atoms/layoutAtom";
 import { cn } from "@/lib/utils";
+import { ConfirmationModal } from "../modals";
+import { RenderIf } from "../shared";
 
 export function NewTabs() {
   const [tabs, setTabs] = useAtom(tabsAtom);
   const setSyncedActiveTab = useSetAtom(syncActiveTabAtom);
   const [activeTab, setActiveTab] = useAtom(activeTabAtom);
 
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
   return (
-    <div className="flex gap-2 items-center flex-1 overflow-hidden w-full">
-      <button
-        type="button"
-        className="h-[32px] w-[32px] flex items-center justify-center rounded-md border border-[#121212]"
-        onClick={() => {
-          const currentTab = {
-            id: Date.now(),
-            label: "untitled layout",
-            name: "Untitled Layout",
-            editMode: false,
-          };
-          setTabs((prev) => {
-            return [...prev, currentTab];
-          });
-          setActiveTab(currentTab);
-        }}
-      >
-        <AddTab />
-      </button>
+    <Fragment>
+      <div className="flex gap-2 items-center flex-1 overflow-hidden w-full">
+        <button
+          type="button"
+          className="h-[32px] w-[32px] flex items-center justify-center rounded-md border border-[#121212]"
+          onClick={() => {
+            const currentTab = {
+              id: Date.now(),
+              label: "untitled layout",
+              name: "Untitled Layout",
+              editMode: false,
+            };
+            setTabs((prev) => {
+              return [...prev, currentTab];
+            });
+            setActiveTab(currentTab);
+          }}
+        >
+          <AddTab />
+        </button>
 
-      <div className="h-[18px] w-[1px] bg-[#141414]"></div>
+        <div className="h-[18px] w-[1px] bg-[#141414]"></div>
 
-      <div className="flex-1 overflow-x-auto flex gap-2 items-center scrollbar pr-2">
-        {tabs.map((item) => {
-          const isActive = activeTab.id === item.id;
-          return (
-            <button
-              key={item.id}
-              type="button"
-              onClick={() => {
-                setActiveTab(item);
-              }}
-            >
-              <div
-                className={cn(
-                  "flex items-center gap-2 w-[160px] h-[32px] px-[6px] rounded-md",
-                  isActive ? "bg-[#252525]" : "bg-[#111]"
-                )}
-              >
-                <TabLayout active={isActive} />
-                <p
-                  className={cn(
-                    "text-xs font-medium flex-1 truncate",
-                    isActive ? "text-white" : "text-[#7a7a7a]"
-                  )}
+        <div className="flex-1 overflow-x-auto flex gap-2 items-center scrollbar pr-2">
+          {tabs.map((item) => {
+            const isActive = activeTab.id === item.id;
+            return (
+              <div key={item.id} className="relative">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setActiveTab(item);
+                  }}
                 >
-                  {item.name}
-                </p>
+                  <div
+                    className={cn(
+                      "flex items-center gap-2 w-[160px] h-[32px] px-2 rounded-md gap-3 justify-between",
+                      isActive ? "bg-[#252525]" : "bg-[#111]"
+                    )}
+                  >
+                    <div className="flex gap-1 items-center">
+                      <TabLayout active={isActive} />
+                      <p
+                        className={cn(
+                          "text-xs font-medium flex-1 truncate flex-1",
+                          isActive ? "text-white" : "text-[#7a7a7a]"
+                        )}
+                      >
+                        {item.name}
+                      </p>
+                    </div>
+
+                    <div className="w-[16px] h-[16px]"></div>
+                  </div>
+                </button>
+                <RenderIf condition={tabs.length > 1}>
+                  <button
+                    type="button"
+                    className="absolute right-[8px] top-[50%] translate-y-[-50%]"
+                    onClick={() => {
+                      setShowDeleteModal(true);
+                    }}
+                  >
+                    <CloseTab />
+                  </button>
+                </RenderIf>
               </div>
-            </button>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+
+      <ConfirmationModal
+        handleCloseModal={() => {
+          setShowDeleteModal(false);
+        }}
+        open={showDeleteModal}
+        title="Delete Exchange liquidation Heat Map?"
+        details="You can always add new widgets to your dashboard after widgets are deleted"
+      />
+    </Fragment>
   );
 }
---
+++ apps/dashboard/src/components/modals/confirmation.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { ModalContainer } from "../shared";
+
+interface IProps {
+  handleCloseModal: () => void;
+  open: boolean;
+  title?: string;
+  details?: string;
+  handleCancel?: () => void;
+  handleConfirm?: () => void;
+}
+
+export function ConfirmationModal(props: IProps) {
+  const {
+    handleCloseModal,
+    open,
+    details,
+    handleCancel,
+    handleConfirm,
+    title,
+  } = props;
+  return (
+    <ModalContainer
+      handleClose={handleCloseModal}
+      open={open}
+      noHeader
+      className="!max-w-[460px] w-full p-0"
+    >
+      <div className="bg-[#090909] border border-[#141414] rounded-[10px] p-6">
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-[10px]">
+            <h4 className="text-white font-medium leading-[24px] text-base">
+              {title}
+            </h4>
+            <p className="text-[#878787] font-medium leading-[1.25] text-sm">
+              {details}
+            </p>
+          </div>
+
+          <div className="flex justify-end gap-2">
+            <button
+              className="text-[#C3C3C3] font-medium leading-[1.25] text-[13px] bg-[#0E0E0E] border border-[#121212] rounded-sm p-2"
+              onClick={handleCloseModal}
+            >
+              Cancel
+            </button>
+
+            <button
+              className="text-[#090909] font-medium leading-[1.25] text-[13px] bg-white border border-[#121212] rounded-sm p-2"
+              onClick={handleConfirm}
+            >
+              Delete Widget
+            </button>
+          </div>
+        </div>
+      </div>
+    </ModalContainer>
+  );
+}
