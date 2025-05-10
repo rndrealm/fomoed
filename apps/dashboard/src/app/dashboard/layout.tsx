df5bdedf9bc17d518e3a23a5e646c1c232a7cfaa
Oladejiraji <rajioladeji2@gmail.com>
2025-05-10 10:58:17 +0100

    fix: vercel build fix

--- apps/dashboard/src/app/dashboard/layout.tsx
+++ apps/dashboard/src/app/dashboard/layout.tsx
@@ -1,22 +1,14 @@
-// import Navbar from "@/components/ui/navbar";
-"use client";
-import { Navbar, RenderIf } from "@/components/shared";
-import { utilsAtom } from "@/lib/atoms/utilsAtom";
-import { useAtomValue } from "jotai";
+import { Navbar } from "@/components/shared";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const utils = useAtomValue(utilsAtom);
-
   return (
     <>
       <div className="absolute inset-x-0 top-0">
-        <RenderIf condition={!utils.isFullScreen}>
-          <Navbar />
-        </RenderIf>
+        <Navbar />
       </div>
       {children}
     </>
