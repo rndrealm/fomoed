35995bd0e301bc5113dafd80671f191653b1d2ca
Navindu Amarakoon <navanavi3@gmail.com>
2025-05-12 12:20:39 +1000

    feat: navbar

--- apps/dashboard/src/app/signals/page.tsx
+++ apps/dashboard/src/app/signals/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import NavBar from "@/components/shared/nav-bar";
 import SmartSignalsMvp from "@/screens/signals/SmartSignalsMvp";
 
 export default function Home() {
   return (
     <div className="bg-black min-h-screen p-2 h-full">
+      <NavBar />
       <SmartSignalsMvp />
     </div>
   );
---
+++ apps/dashboard/src/components/shared/nav-bar.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import FomoedIcon from "../icons/FomoedIcon";
+import NavBarDashboardIcon from "../icons/NavBarDashboardIcon";
+import NavBarNewsIcon from "../icons/NavBarNewsIcon";
+import NavBarSignalsIcon from "../icons/NavBarSignalsIcon";
+
+const links = [
+  {
+    name: "Dashboard",
+    icon: <NavBarDashboardIcon />,
+    path: "/dashboard",
+  },
+  {
+    name: "News",
+    icon: <NavBarNewsIcon />,
+    path: "/news",
+  },
+  {
+    name: "Signals",
+    icon: <NavBarSignalsIcon />,
+    path: "/signals",
+  },
+];
+
+const NavBar = () => {
+  const path = usePathname();
+  return (
+    <div className="w-full max-w-[590px] h-[60px] mx-auto mt-4 p-[0.4px] rounded-[15px] nav-bar-gradient">
+      <div className="flex items-center h-full w-full rounded-[15px] bg-background px-6 py-3.5">
+        <FomoedIcon />
+        <div className="flex items-center gap-4 ml-6">
+          {links.map((link) => (
+            <a
+              href={link.path}
+              key={link.name}
+              className={cn(
+                "flex items-center gap-2 text-sm font-medium text-white",
+                path === link.path ? "text-white" : "text-[#737373]"
+              )}
+            >
+              {link.icon}
+              <span className="font-medium">{link.name}</span>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
