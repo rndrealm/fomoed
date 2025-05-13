aed1c105f0979fce319f6bc8fba41a948568c465
Oladejiraji <rajioladeji2@gmail.com>
2025-05-13 07:54:14 +0100

    fix: og iage

--- apps/fomoed/src/routes/(app)/+layout.svelte
+++ apps/fomoed/src/routes/(app)/+layout.svelte
@@ -115,7 +115,8 @@ sentiment analysis"
 		title: `Fomoed`,
 		description:
 			'Fomoed provides a toolset for effortlessly navigating the emotional rollercoaster that is crypto. Have complete access to your favorite Altcoins and get precise data-based market sentiment analysis',
-		image: '/og.png',
+		image:
+			'https://firebasestorage.googleapis.com/v0/b/deji-firegram.appspot.com/o/og.png?alt=media&token=367d845c-0fb4-4c0a-9ca9-b51567c3944a',
 		imageAlt: `Fomoed`
 	}}
 />
