a49c536065afe3e5bbc0180822cd9a748e1e18a0
JakubBlaha-IDS <jakubblaha@ids.company>
2025-05-13 10:05:30 +0200

    fix: enable login through whole domain

--- apps/fomoed/src/ts/utils/server/supabase.ts
+++ apps/fomoed/src/ts/utils/server/supabase.ts
@@ -1,10 +1,17 @@
-import { PRIVATE_SUPABASE_SECRET } from "$env/static/private";
-import { PUBLIC_SUPABASE_URL } from "$env/static/public";
-import { createServerClient } from "@supabase/ssr";
-import type { SupabaseClient } from "@supabase/supabase-js";
+import { PRIVATE_SUPABASE_SECRET } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { createServerClient } from '@supabase/ssr';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
 export function getServerClient(): SupabaseClient {
 	return createServerClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SECRET, {
-		cookies: {}
+		cookies: {},
+		cookieOptions: {
+			domain: '.fomoed.io',
+			path: '/',
+			sameSite: 'lax',
+			secure: true,
+			maxAge: 60 * 60 * 24 * 30 // 30 days
+		}
 	});
 }
