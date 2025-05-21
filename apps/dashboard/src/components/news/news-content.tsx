2267670aabfb608067fb7e414d8c7d084ee8722e
Oladejiraji <rajioladeji2@gmail.com>
2025-05-21 08:11:46 +0100

    fix: vercel server error

--- apps/dashboard/src/components/news/news-content.tsx
+++ apps/dashboard/src/components/news/news-content.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import parse from "html-react-parser";
 import { Footer } from "./footer";
@@ -14,7 +15,7 @@ export function NewsContent(props: IProps) {
   return (
     <div className="text-white flex flex-col gap-10 max-w-[640px] mx-auto">
       <div className="flex flex-col gap-6">
-        <div className="flex justify-between flex-col sm:flex-row gap-2">
+        <div className="flex flex-col justify-between gap-2 sm:flex-row">
           <div className="flex items-center gap-2">
             <p className="font-medium text-[#9b9b9b] text-[15px] leading-[1.35]">
               20th May, 2025 by
@@ -31,7 +32,7 @@ export function NewsContent(props: IProps) {
             24 MIN READ
           </p>
         </div>
-        <div className="app_news_content flex flex-col gap-4">
+        <div className="flex flex-col gap-4 app_news_content">
           {parse(normalizedContent)}
         </div>
       </div>
