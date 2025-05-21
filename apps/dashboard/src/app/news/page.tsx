cc874c65fe1803ba914d2c44ec0ea1243e7ab5c8
Oladejiraji <rajioladeji2@gmail.com>
2025-05-21 10:04:17 +0100

    fix: vercel server error

---
+++ apps/dashboard/src/app/news/page.tsx
@@ -0,0 +1,51 @@
+import { NewsContent } from "@/components/news";
+import { normalizeHtmlText } from "@/lib/utils";
+import React, { Fragment } from "react";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const url = new URL(
+    `/api/newslab/single-post?id=${id}`,
+    "https://dashboard-dev.fomoed.io"
+  );
+
+  const res = await fetch(url);
+  const post = await res.json();
+
+  const normalizedTitle = normalizeHtmlText(post.title);
+  return {
+    title: "Fomoed News",
+    description: normalizedTitle,
+    metadataBase: new URL("https://dashboard-dev.fomoed.io"),
+    openGraph: {
+      images: "/og3.png",
+    },
+  };
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Promise<{ id: string }>;
+}) {
+  const id = (await searchParams)?.id || "";
+
+  const url = new URL(
+    `/api/newslab-posts/${id}`,
+    process.env.PUBLIC_NEWSLAB_URL
+  );
+
+  const res = await fetch(url);
+
+  const text = await res.text();
+
+  return (
+    <div className="pt-[96px] bg-[#0C0C0C] h-screen overflow-auto pt-10 pb-14 px-4">
+      <NewsContent content={text} />
+    </div>
+  );
+}
