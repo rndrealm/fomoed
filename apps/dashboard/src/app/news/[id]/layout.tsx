8af4139df54f0c4dda21c52ab11151f9b885264f
Oladejiraji <rajioladeji2@gmail.com>
2025-05-21 10:20:43 +0100

    fix: vercel server error

---
+++ apps/dashboard/src/app/news/[id]/layout.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "@/components/shared";
+import { Fragment } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Fragment>
+      <div className="absolute inset-x-0 top-0">
+        <Navbar isNews />
+      </div>
+      {children}
+    </Fragment>
+  );
+}
