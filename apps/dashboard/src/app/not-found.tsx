db528f313677a02189116fed37369d9b10fd890c
Tobos17 <stoulil.tobias@gmail.com>
2025-06-12 12:53:28 +0200

    error fix

---
+++ apps/dashboard/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import ErrorComponent from "@/components/shared/error-component";
+
+const content = {
+    error: "404",
+    message: "Oops! Nothing here.",
+    message2: "Looks like the page you're trying to view does not exist",
+    label: "Go Back Home",
+};
+
+export default function NotFound() {
+    return <ErrorComponent content={content} />;
+}
