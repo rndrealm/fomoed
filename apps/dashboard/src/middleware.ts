137c3e254976c8e2bbf13ab4b998398826d029dc
Oladejiraji <rajioladeji2@gmail.com>
2025-06-17 14:13:31 +0100

    fix: supabase auth issue

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -3,29 +3,29 @@ import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "./lib/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+  // const response = NextResponse.next({
+  //   request: {
+  //     headers: request.headers,
+  //   },
+  // });
 
-  const supabase = await createSupabaseReqResClient(request, response);
+  // const supabase = await createSupabaseReqResClient(request, response);
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
   // Authenticate API routes
   //TODO:  move this to indifidual API routes
-  if (
-    !user &&
-    request.nextUrl.pathname.startsWith("/api") &&
-    !request.nextUrl.pathname.includes("/news") &&
-    !request.nextUrl.pathname.includes("/scrape-cfgi") &&
-    !request.nextUrl.pathname.includes("/newslab")
-  ) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  // if (
+  //   !user &&
+  //   request.nextUrl.pathname.startsWith("/api") &&
+  //   !request.nextUrl.pathname.includes("/news") &&
+  //   !request.nextUrl.pathname.includes("/scrape-cfgi") &&
+  //   !request.nextUrl.pathname.includes("/newslab")
+  // ) {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
 
   return await updateSession(request);
 }
