61355e87bfc2f5f9fb92aef276fbb9fe034a0c1c
Oladejiraji <rajioladeji2@gmail.com>
2025-07-09 13:57:19 +0100

    fix: reverted supported pair functions

--- apps/dashboard/src/app/api/supported-exchange-pairs/route.ts
+++ apps/dashboard/src/app/api/supported-exchange-pairs/route.ts
@@ -1,11 +1,29 @@
-import { fetchCoinglassSupportedPairs } from "@/lib/server/utils/coinglass";
 import { NextResponse } from "next/server";
 
 //! REQUEST HANDLER for /api/supported-exchange-pairs
 export async function GET() {
-  const supportedPairs = await fetchCoinglassSupportedPairs();
+  try {
+    //Todo move this api call to another function
+    const url = "https://open-api-v4.coinglass.com/api/futures/supported-exchange-pairs";
 
-  if (!supportedPairs) {
-    return NextResponse.json({ error: "Failed to fetch supported pairs" }, { status: 500 });
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "CG-API-KEY": process.env.PRIVATE_COINGLASS_KEY as string,
+      },
+      cache: "no-store",
+    });
+    const data = await res.json();
+
+    if (!res.ok || !data.data) {
+      return NextResponse.json({ error: "Failed to fetch CFGI data" }, { status: 500 });
+    }
+
+    return NextResponse.json({ data: data.data });
+  } catch (error) {
+    // Handle errors gracefully
+    console.log("Error fetching CFGI data:", error);
+    return NextResponse.json({ error: "Failed to fetch CFGI data" }, { status: 500 });
   }
 }
