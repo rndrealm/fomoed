f38de76cd7fbada1e6a4ee7f48c68c42335d3ca0
Tobos17 <stoulil.tobias@gmail.com>
2025-07-15 14:23:13 +0200

    news feed

---
+++ apps/dashboard/src/app/news/page.tsx
@@ -0,0 +1,16 @@
+
+import React, { Fragment } from "react";
+import { SearchPopup } from "./SearchPopup";
+
+export default async function Page() {
+
+    return (
+        <div className=" bg-black pt-[86px]">
+            <div className="relative flex flex-col w-full h-full px-6">
+
+                <SearchPopup />
+            </div>
+
+        </div>
+    );
+}
