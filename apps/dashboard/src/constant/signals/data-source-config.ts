9a3a37ba0264f17f59d8e90360ae5e489bae6afe
JakubBlaha-IDS <jakubblaha@ids.company>
2025-07-18 23:24:56 +0200

    fix: price data source config

--- apps/dashboard/src/constant/signals/data-source-config.ts
+++ apps/dashboard/src/constant/signals/data-source-config.ts
@@ -97,7 +97,7 @@ export const topicSelectorMap: Record<
   santiment_rsi_7d: {
     component: TopicSelectorSymbol,
   },
-  price: {
+  ticker: {
     component: TopicSelectorSymbol,
   },
   weighted_sentiment: {
