ad0afa178579c2a4e9633dd53966d53a6a9131a4
Tobos17 <stoulil.tobias@gmail.com>
2025-07-19 21:13:35 +0200

    scroll thing

--- apps/dashboard/src/app/news/page.tsx
+++ apps/dashboard/src/app/news/page.tsx
@@ -3,14 +3,8 @@ import { NewsSection } from "./NewsSection";
 
 export default async function Page() {
   return (
-    <div className="bg-black pt-[64px] md:pt-[86px]">
-      <div className="relative flex h-full w-full flex-col px-6">
-        <div className="ml-11">
-          <Suspense>
-            <NewsSection />
-          </Suspense>
-        </div>
-      </div>
-    </div>
+    <Suspense>
+      <NewsSection />
+    </Suspense>
   );
 }
