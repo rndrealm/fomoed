5becaa761c92d3631b964da5d69d1433fb37e685
Tobos17 <stoulil.tobias@gmail.com>
2025-08-04 18:02:33 +0200

    fix overflow and center image

--- apps/dashboard/src/app/auth/layout.tsx
+++ apps/dashboard/src/app/auth/layout.tsx
@@ -7,8 +7,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative bg-black">
-      <div className="absolute inset-0 h-screen w-[105vw]">
+    <div className="relative bg-black w-full overflow-hidden">
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 h-screen w-[105vw]">
         <Image src={authAssets.BgIllustration} alt="Auth page pattern" fill />
       </div>
       {children}
