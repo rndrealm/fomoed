3fbd6180a3125053a513cb5877e2649b2f62bc35
JakubBlaha-IDS <jakubblaha@ids.company>
2025-08-25 22:02:12 +0200

    fix: incorrect import

--- apps/dashboard/src/app/(app)/pricing/page.tsx
+++ apps/dashboard/src/app/(app)/pricing/page.tsx
@@ -11,7 +11,7 @@ import useSubscription, {
   subscriptionStateToConfig,
 } from "@/hooks/subscription";
 import type { PriceLookupKey } from "@/lib/plans/plans.types";
-import { UserSubscriptionsResponseData } from "../api/subscriptions/route";
+import { UserSubscriptionsResponseData } from "../../api/subscriptions/route";
 import { BellIcon, Grid2X2Icon, GridIcon, LucidePuzzle, Puzzle, PuzzleIcon } from "lucide-react";
 import { WidgetDashboardIcon } from "@/components/icons/icons";
 
