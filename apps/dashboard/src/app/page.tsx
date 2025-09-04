5224d1cfab9232a435f20a1ca1120b499649cb0c
Seun Oyediran <oyediran@ids.company>
2025-09-04 11:01:38 +0100

    fix: app landing flow

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,5 +1,6 @@
+import { AppRoutes } from "@/lib/routes";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  return redirect("/news");
+  return redirect(AppRoutes.dashboard.path);
 }
