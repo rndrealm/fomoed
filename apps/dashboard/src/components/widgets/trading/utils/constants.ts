ad7b7c369e94c49aeb17c09e8e15045970e43102
oladeji <oladeji@ids.company>
2025-12-11 16:03:57 +0100

    fix: testnet env name

--- apps/dashboard/src/components/widgets/trading/utils/constants.ts
+++ apps/dashboard/src/components/widgets/trading/utils/constants.ts
@@ -3,6 +3,6 @@ export const SPOT_MAX_DECIMALS = 8;
 
 export const MAX_PRICE_SF = 5;
 
-export const isTestnet = process.env.HYPERLIQUID_IS_TESTNET || true;
+export const isTestnet = process.env.NEXT_PUBLIC_HYPERLIQUID_IS_TESTNET || true;
 
 export const HYPERLIQUID_BASE_URL = isTestnet ? "https://api.hyperliquid-testnet.xyz" : "https://api.hyperliquid.xyz";
