acb31bbc0f530ed319e6f7d1c07c4915dd64b87a
MarioIDS <mario@ids.company>
2026-01-12 21:03:17 +0700

    Removing event photo page

--- apps/dashboard/src/lib/routes.ts
+++ apps/dashboard/src/lib/routes.ts
@@ -71,10 +71,10 @@ export const AppRoutes = {
     name: "Terms of Service",
     path: "/terms-of-service",
   },
-  eventPhotos: {
-    name: "Event Photos",
-    path: "/event-photos",
-  },
+  // eventPhotos: {
+  //   name: "Event Photos",
+  //   path: "/event-photos",
+  // },
   waitlist: {
     name: "Waitlist",
     path: "/waitlist",
